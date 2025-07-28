6fe43ed81262d39f48f78563a357b73ab696df6a
Liam Kyle Viray <i_liamkyle.viray@stratpoint.com>
2025-07-28 17:19:18 +0800

    feat: Updated ui of sign in to match theme more

--- project/app/(auth)/sign-in/[[...sign-in]]/page.tsx
+++ project/app/(auth)/sign-in/[[...sign-in]]/page.tsx
@@ -8,16 +8,26 @@ export default function SignInPage() {
         <SignIn
           appearance={{
             elements: {
-              socialButtonsBlockButton: "bg-background py-2 hover:bg-accent",
+              card: "!p-6 gap-6",
+              main: "gap-5",
+              form: "gap-5",
+              headerTitle: "text-2xl text-left",
+              headerSubtitle: "text-left",
+              formFieldInput: "py-1.5",
+              socialButtonsBlockButtonText: "text-white text-[15px]",
+              socialButtonsBlockButton:
+                "bg-background py-2 hover:bg-accent transition-colors duration-200",
+              formButtonPrimary: "bg-primary text-white hover:bg-primary/90",
             },
             variables: {
               colorMuted: "var(--card)",
+              colorPrimary: "var(--primary)",
             },
           }}
         />
       </ClerkLoaded>
       <ClerkLoading>
-        <Loader2 className="animate-spin text-muted-foreground" />
+        <Loader2 className="animate-spin size-20 text-muted-foreground" />
       </ClerkLoading>
     </div>
   );
