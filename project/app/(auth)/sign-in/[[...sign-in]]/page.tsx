9cc9715883bd7360d35f22615d125cbe33a867c9
Liam Kyle Viray <i_liamkyle.viray@stratpoint.com>
2025-07-28 16:19:25 +0800

    feat: Updated styles for social buttons

--- project/app/(auth)/sign-in/[[...sign-in]]/page.tsx
+++ project/app/(auth)/sign-in/[[...sign-in]]/page.tsx
@@ -3,21 +3,22 @@ import { Loader2 } from "lucide-react";
 
 export default function SignInPage() {
   return (
-    <div className="min-h-screen grid grid-cols-1 lg:grid-cols-2">
-      <div className="flex items-center justify-center mt-8 w-full">
-        <ClerkLoaded>
-          <SignIn
-            appearance={{
-              variables: {
-                colorMuted: "var(--card)",
-              },
-            }}
-          />
-        </ClerkLoaded>
-        <ClerkLoading>
-          <Loader2 className="animate-spin text-muted-foreground" />
-        </ClerkLoading>
-      </div>
+    <div className="flex items-center justify-center -mt-8 w-full min-h-screen">
+      <ClerkLoaded>
+        <SignIn
+          appearance={{
+            elements: {
+              socialButtonsBlockButton: "bg-background py-2 hover:bg-accent",
+            },
+            variables: {
+              colorMuted: "var(--card)",
+            },
+          }}
+        />
+      </ClerkLoaded>
+      <ClerkLoading>
+        <Loader2 className="animate-spin text-muted-foreground" />
+      </ClerkLoading>
     </div>
   );
 }
