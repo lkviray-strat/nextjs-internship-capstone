04a1cbdfea38bf2355d91fc6e2810a0cf3d9e1b3
Liam Kyle Viray <i_liamkyle.viray@stratpoint.com>
2025-07-30 01:57:13 +0800

    chore: Inferred schema base types and added type util

--- project/lib/utils.ts
+++ project/lib/utils.ts
@@ -1,6 +1,8 @@
-import { clsx, type ClassValue } from "clsx"
-import { twMerge } from "tailwind-merge"
+import { clsx, type ClassValue } from "clsx";
+import { twMerge } from "tailwind-merge";
 
 export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs))
+  return twMerge(clsx(inputs));
 }
+
+export type WithRelations<T, R extends Record<string, unknown>> = T & R;
