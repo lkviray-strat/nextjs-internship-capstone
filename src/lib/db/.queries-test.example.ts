57ff66f1fc9d0717e1c89619f6a592fb91b4c94b
Liam Viray <i_liamkyle.viray@stratpoint.com>
2025-08-01 17:57:38 +0800

    chore: Turned queries test into a example for .gitignore

---
+++ src/lib/db/.queries-test.example.ts
@@ -0,0 +1,23 @@
+import "dotenv/config";
+
+// Copy this file to `queries-test.ts` to run manual tests
+// cp .example.test-queries.ts queries-test.ts
+
+async function main() {
+  console.log("Running sample queries...");
+  // Enter your queries below ===========================
+
+  // Uncomment the queries you want to run
+  // const users = await queries.users.getAllUsers();
+  // console.log("All users:", users);
+
+  // const liam = await queries.users.getUsersByEmail("liam@example.com");
+  // console.log("User with email:", liam);
+
+  // ====================================================
+  console.log("Sample queries completed successfully!");
+}
+
+main().catch((err) => {
+  console.error("❌ Error running query:", err);
+});
