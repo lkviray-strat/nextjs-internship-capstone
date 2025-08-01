c248378fbb6c4f08cfe55fc3297d234588d625af
Liam Viray <i_liamkyle.viray@stratpoint.com>
2025-08-01 14:29:41 +0800

    chore: Added websockets support to db

--- src/lib/db/index.ts
+++ src/lib/db/index.ts
@@ -1,16 +1,19 @@
 // db/index.ts
-import { neon } from "@neondatabase/serverless";
-import { drizzle as drizzleNeon } from "drizzle-orm/neon-http";
+import { Pool as neonPool } from "@neondatabase/serverless";
+import { drizzle as drizzleNeon } from "drizzle-orm/neon-serverless";
 import { drizzle as drizzlePg } from "drizzle-orm/node-postgres";
-import { Pool } from "pg";
+import { Pool as pgPool } from "pg";
 import * as schema from "./schema";
 
 export const POSTGRES_DATABASE_URL = `postgres://${process.env.POSTGRES_USER}:${process.env.POSTGRES_PASSWORD}@${process.env.POSTGRES_HOST}:${process.env.POSTGRES_PORT}/${process.env.POSTGRES_DB}`;
 
 export const db =
   process.env.USE_NEON?.toLowerCase() === "true"
-    ? drizzleNeon(neon(process.env.NEON_DATABASE_URL!), { schema })
-    : drizzlePg(new Pool({ connectionString: POSTGRES_DATABASE_URL }), {
+    ? drizzleNeon(
+        new neonPool({ connectionString: process.env.NEON_DATABASE_URL! }),
+        { schema }
+      )
+    : drizzlePg(new pgPool({ connectionString: POSTGRES_DATABASE_URL }), {
         schema,
       });
 
