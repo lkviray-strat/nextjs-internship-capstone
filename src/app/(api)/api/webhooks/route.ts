02c01670261099ba090f6d0cc1c95f0217ca1892
Liam Kyle Viray <i_liamkyle.viray@stratpoint.com>
2025-08-05 17:34:50 +0800

    chore: Updated api/webhooks POST to catch events and synch DB

--- src/app/(api)/api/webhooks/route.ts
+++ src/app/(api)/api/webhooks/route.ts
@@ -1,20 +1,44 @@
-import { verifyWebhook } from "@clerk/nextjs/webhooks";
+import {
+  createUserAction,
+  deleteUserAction,
+  updateUserAction,
+} from "@/src/use/actions/user-actions";
+import { verifyWebhook, WebhookEvent } from "@clerk/nextjs/webhooks";
 import { NextRequest } from "next/server";
 
 export async function POST(req: NextRequest) {
   try {
-    const evt = await verifyWebhook(req);
-    const { id } = evt.data;
-    const eventType = evt.type;
-
-    console.log(
-      `Received webhook with ID ${id} and event type of ${eventType}`
-    );
-    console.log("Webhook payload:", evt.data);
-
+    const evt = (await verifyWebhook(req)) as WebhookEvent;
+    console.log("\n📩 Webhook event received:", evt.type);
+    switch (evt.type) {
+      case "user.created":
+        const createResult = await createUserAction(evt.data);
+        if (!createResult?.success) {
+          console.error("❌ Error creating user:", createResult?.error, "\n");
+          return new Response("Error creating user", { status: 400 });
+        }
+        console.log("✅ User created with ID:", evt.data.id, "\n");
+        break;
+      case "user.updated":
+        const updateResult = await updateUserAction(evt.data);
+        if (!updateResult?.success) {
+          console.error("❌ Error updating user:", updateResult?.error, "\n");
+          return new Response("Error updating user", { status: 400 });
+        }
+        console.log("✅ User updated with ID:", evt.data.id, "\n");
+        break;
+      case "user.deleted":
+        const deleteResult = await deleteUserAction(evt.data.id as string);
+        if (!deleteResult?.success) {
+          console.error("❌ Error deleting user:", deleteResult?.error, "\n");
+          return new Response("Error deleting user", { status: 400 });
+        }
+        console.log("✅ User deleted with ID:", evt.data.id, "\n");
+        break;
+    }
     return new Response("Webhook received", { status: 200 });
   } catch (err) {
-    console.error("Error verifying webhook:", err);
+    console.error("🚫 Error verifying webhook:", err);
     return new Response("Error verifying webhook", { status: 400 });
   }
 }
