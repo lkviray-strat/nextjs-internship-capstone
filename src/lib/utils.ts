383b35e6756879a43370b774231f11d8e779c26a
Liam Kyle Viray <i_liamkyle.viray@stratpoint.com>
2025-08-05 17:41:03 +0800

    chore: Abstracted away logging for reusability

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -6,3 +6,19 @@ export function cn(...inputs: ClassValue[]) {
 }
 
 export type WithRelations<T, R extends Record<string, unknown>> = T & R;
+
+export function logWebhookEvent(
+  status: "success" | "error",
+  action: string,
+  data?: unknown
+) {
+  const symbols = {
+    success: "✅",
+    error: "❌",
+  };
+
+  const message = `${symbols[status]} ${action}`;
+  const logFn = status === "success" ? console.log : console.error;
+
+  logFn(message, data ?? "", "\n");
+}
