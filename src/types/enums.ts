b964ba2069942039f4fd15ab952f6a204ad4bbd7
Liam Kyle Viray <i_liamkyle.viray@stratpoint.com>
2025-08-12 18:27:33 +0800

    chore: Updated types for permission actions and resources

--- src/types/enums.ts
+++ src/types/enums.ts
@@ -1,4 +1,13 @@
-import { projectStatusEnum, taskPriorityEnum } from "../lib/db/enums";
+import {
+  permissionActionsEnum,
+  permissionResourcesEnum,
+  projectStatusEnum,
+  taskPriorityEnum,
+} from "../lib/db/enums";
 
 export type TaskPriorityEnum = (typeof taskPriorityEnum.enumValues)[number];
 export type ProjectStatusEnum = (typeof projectStatusEnum.enumValues)[number];
+export type PermissionActionsEnum =
+  (typeof permissionActionsEnum.enumValues)[number];
+export type PermissionResourcesEnum =
+  (typeof permissionResourcesEnum.enumValues)[number];
