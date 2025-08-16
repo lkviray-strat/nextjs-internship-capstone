e5ef0407274a990eeda471042d3ed8ec028c129a
Liam Viray <i_liamkyle.viray@stratpoint.com>
2025-08-17 01:05:40 +0800

    chore: Added trpc route for get teams by leader id

--- trpc/routers/_app.ts
+++ trpc/routers/_app.ts
@@ -2,6 +2,12 @@ import { queries } from "@/src/lib/db/queries";
 import { createTRPCRouter, protectedProcedure } from "../init";
 
 export const appRouter = createTRPCRouter({
+  teams: {
+    getMyTeams: protectedProcedure.query(async ({ ctx }) => {
+      return await queries.teams.getTeamsByLeaderId(ctx.auth.userId as string);
+    }),
+  },
+
   getProject: protectedProcedure.query(async () => {
     return await queries.projects.getRecentProjects(3);
   }),
