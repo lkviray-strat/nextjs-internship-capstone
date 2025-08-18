7374c3f0eb5d8ad77f336b2229871e713e75070c
Liam Kyle Viray <i_liamkyle.viray@stratpoint.com>
2025-08-19 03:33:13 +0800

    feat: Added assemble navigation on navbar

--- src/components/landing-navbar.tsx
+++ src/components/landing-navbar.tsx
@@ -6,31 +6,43 @@ import { buttonVariants } from "./ui/button";
 import { UserProfileButton } from "./user-profile-button";
 
 type LandingNavbar = {
+  showNavs?: boolean;
   buttonHref: (path: string) => string;
 };
 
-export function LandingNavbar({ buttonHref }: LandingNavbar) {
+export function LandingNavbar({ buttonHref, showNavs = true }: LandingNavbar) {
   return (
     <div className="hidden sm:flex justify-between items-center gap-10">
-      <div className="flex items-center justify-center gap-7">
-        <HoverUnderline>
-          <Link
-            href={buttonHref("dashboard")}
-            className={`text-[16px] rounded-lg`}
-          >
-            Dashboard
-          </Link>
-        </HoverUnderline>
+      {showNavs && (
+        <div className="flex items-center justify-center gap-7">
+          <HoverUnderline>
+            <Link
+              href="/assemble"
+              className={`text-[16px] rounded-lg`}
+            >
+              Assemble
+            </Link>
+          </HoverUnderline>
 
-        <HoverUnderline>
-          <Link
-            href={buttonHref("projects")}
-            className={`text-[16px] rounded-lg`}
-          >
-            Projects
-          </Link>
-        </HoverUnderline>
-      </div>
+          <HoverUnderline>
+            <Link
+              href={buttonHref("dashboard")}
+              className={`text-[16px] rounded-lg`}
+            >
+              Dashboard
+            </Link>
+          </HoverUnderline>
+
+          <HoverUnderline>
+            <Link
+              href={buttonHref("projects")}
+              className={`text-[16px] rounded-lg`}
+            >
+              Projects
+            </Link>
+          </HoverUnderline>
+        </div>
+      )}
 
       <div className="flex items-center justify-center gap-4">
         <UserProfileButton />
