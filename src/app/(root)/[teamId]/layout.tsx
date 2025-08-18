fba66491cce7b4ea3fd9a45be6705fb09f62fd1b
Liam Kyle Viray <i_liamkyle.viray@stratpoint.com>
2025-08-18 23:39:00 +0800

    chore: Implemented reset queries on change of teams

--- src/app/(root)/[teamId]/layout.tsx
+++ src/app/(root)/[teamId]/layout.tsx
@@ -1,7 +1,9 @@
 import { MainNavbar } from "@/src/components/main-navbar";
 import { MainSidebar } from "@/src/components/main-sidebar";
+import { ResetProvider } from "@/src/components/reset-provider";
 import { SidebarInset, SidebarProvider } from "@/src/components/ui/sidebar";
 import { queries } from "@/src/lib/db/queries";
+import { auth } from "@clerk/nextjs/server";
 import { notFound } from "next/navigation";
 import { Suspense } from "react";
 
@@ -16,28 +18,36 @@ export default async function MainLayout({
   params,
   children,
 }: MainLayoutProps) {
+  const { userId } = await auth();
   const { teamId } = await params;
-  const existingTeam = await queries.teams.getTeamsById(teamId);
+  const team = await queries.teams.getTeamsById(teamId);
+  const teamMember = await queries.teamMembers.getTeamMembersByIds(
+    userId!,
+    teamId
+  );
+  const isValid = team.length > 0 && teamMember.length > 0;
 
-  if (existingTeam.length === 0) {
+  if (!isValid) {
     return notFound();
   }
 
   return (
-    <SidebarProvider>
-      <div className="flex min-h-screen w-full">
-        <MainSidebar />
-        {/* py-3 pr-3 if use inset variant */}
-        <div className="flex-1 py-3">
-          <SidebarInset>
-            <MainNavbar />
-            {/* Add min-h-[calc(100vh-4rem)] if use inset variant */}
-            <main className="py-8 px-4 sm:px-6 lg:px-8 ">
-              <Suspense>{children}</Suspense>
-            </main>
-          </SidebarInset>
+    <ResetProvider initialTeamId={teamId}>
+      <SidebarProvider>
+        <div className="flex min-h-screen w-full">
+          <MainSidebar />
+          {/* py-3 pr-3 if use inset variant */}
+          <div className="flex-1 py-3">
+            <SidebarInset>
+              <MainNavbar />
+              {/* Add min-h-[calc(100vh-4rem)] if use inset variant */}
+              <main className="py-8 px-4 sm:px-6 lg:px-8 ">
+                <Suspense>{children}</Suspense>
+              </main>
+            </SidebarInset>
+          </div>
         </div>
-      </div>
-    </SidebarProvider>
+      </SidebarProvider>
+    </ResetProvider>
   );
 }
