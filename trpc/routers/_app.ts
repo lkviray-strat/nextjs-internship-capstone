4ddf5de865b2bd87a90645c74386dfc492219394
Liam Kyle Viray <i_liamkyle.viray@stratpoint.com>
2025-08-19 22:59:21 +0800

    chore: Created get projects with filter trpc, queries, and validations

--- trpc/routers/_app.ts
+++ trpc/routers/_app.ts
@@ -1,7 +1,5 @@
-import { queries } from "@/src/lib/db/queries";
-import { TRPCError } from "@trpc/server";
-import z from "zod";
-import { createTRPCRouter, protectedProcedure } from "../init";
+import { createTRPCRouter } from "../init";
+import { projectRouter } from "./project-routers";
 import { roleRouter } from "./role-routers";
 import { teamMemberRouter } from "./team-member-routers";
 import { teamRouter } from "./team-routers";
@@ -12,29 +10,5 @@ export const appRouter = createTRPCRouter({
   teams: teamRouter,
   teamMembers: teamMemberRouter,
   roles: roleRouter,
-  projects: {
-    getMyRecentProjects: protectedProcedure
-      .input(
-        z.object({
-          teamId: z.guid(),
-        })
-      )
-      .query(async ({ ctx, input }) => {
-        const member = await queries.teamMembers.getTeamMembersByIds(
-          ctx.auth.userId,
-          input.teamId
-        );
-        if (member.length === 0) {
-          throw new TRPCError({
-            code: "FORBIDDEN",
-            message: "You are not a member of this team.",
-          });
-        }
-
-        return await queries.projects.getProjectsByTeamIdLimitAsc(
-          3,
-          input.teamId
-        );
-      }),
-  },
+  projects: projectRouter,
 });
