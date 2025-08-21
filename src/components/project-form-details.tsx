88f9bd8bfa464b52b1d6267e30c9920a8a81373f
Liam Kyle Viray <i_liamkyle.viray@stratpoint.com>
2025-08-22 03:58:25 +0800

    chore: Abstracted project create form into components

---
+++ src/components/project-form-details.tsx
@@ -0,0 +1,58 @@
+import type { useForm } from "react-hook-form";
+import type { CreateProjectRequestInput } from "../types";
+import { RequiredLabel } from "./required-label";
+import {
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "./ui/form";
+import { Input } from "./ui/input";
+import { Textarea } from "./ui/textarea";
+
+type ProjectFormDetailsProps = {
+  control: ReturnType<typeof useForm<CreateProjectRequestInput>>["control"];
+};
+
+export function ProjectFormDetails({ control }: ProjectFormDetailsProps) {
+  return (
+    <>
+      <FormField
+        control={control}
+        name="name"
+        render={({ field }) => (
+          <FormItem className="space-y-0">
+            <FormLabel>
+              <RequiredLabel>Project Name</RequiredLabel>
+            </FormLabel>
+            <FormControl>
+              <Input
+                placeholder="Enter project name (e.g. Website Redesign)"
+                {...field}
+              />
+            </FormControl>
+            <FormMessage />
+          </FormItem>
+        )}
+      />
+      <FormField
+        control={control}
+        name="description"
+        render={({ field }) => (
+          <FormItem>
+            <FormLabel>Description</FormLabel>
+            <FormControl>
+              <Textarea
+                placeholder="Enter project description (e.g. This project aims to redesign the company website)"
+                rows={4}
+                {...field}
+              />
+            </FormControl>
+            <FormMessage />
+          </FormItem>
+        )}
+      />
+    </>
+  );
+}
