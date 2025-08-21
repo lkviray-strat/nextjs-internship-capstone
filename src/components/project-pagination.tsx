1a728eac459faa41d293dc707e6213ff678759b1
Liam Kyle Viray <i_liamkyle.viray@stratpoint.com>
2025-08-22 04:01:23 +0800

    feat: Added pagination and updated query for it

---
+++ src/components/project-pagination.tsx
@@ -0,0 +1,90 @@
+import { projectFiltersToSearchParams } from "../lib/utils";
+import type { ProjectFilters } from "../types";
+import {
+  Pagination,
+  PaginationContent,
+  PaginationEllipsis,
+  PaginationItem,
+  PaginationLink,
+  PaginationNext,
+  PaginationPrevious,
+} from "./ui/pagination";
+
+type ProjectPaginationProps = {
+  filters: Omit<ProjectFilters, "teamId">;
+  totalPages: number;
+};
+
+export function ProjectPagination({
+  filters,
+  totalPages,
+}: ProjectPaginationProps) {
+  const { page, ...otherFilters } = filters;
+
+  const buildQueryString = (targetPage: number) => {
+    const params = projectFiltersToSearchParams({
+      ...otherFilters,
+      page: targetPage,
+    });
+    return params.toString();
+  };
+
+  return (
+    <Pagination>
+      <PaginationContent className="mt-10">
+        {page > 1 && (
+          <>
+            <PaginationItem>
+              <PaginationPrevious
+                href={`/projects?${buildQueryString(page - 1)}`}
+              />
+            </PaginationItem>
+            {page > 2 && (
+              <PaginationItem>
+                <PaginationEllipsis />
+              </PaginationItem>
+            )}
+            <PaginationItem>
+              <PaginationLink
+                isActive={false}
+                href={`/projects?${buildQueryString(page - 1)}`}
+              >
+                {page - 1}
+              </PaginationLink>
+            </PaginationItem>
+          </>
+        )}
+        <PaginationItem>
+          <PaginationLink
+            isActive
+            href={`/projects?${buildQueryString(page)}`}
+          >
+            {page}
+          </PaginationLink>
+        </PaginationItem>
+
+        {page < totalPages && (
+          <>
+            <PaginationItem>
+              <PaginationLink href={`/projects?${buildQueryString(page + 1)}`}>
+                {page + 1}
+              </PaginationLink>
+            </PaginationItem>
+
+            {page < totalPages - 1 && (
+              <PaginationItem>
+                <PaginationEllipsis />
+              </PaginationItem>
+            )}
+
+            <PaginationItem>
+              <PaginationNext
+                href={`/projects?${buildQueryString(page + 1)}`}
+              />
+            </PaginationItem>
+          </>
+        )}
+      </PaginationContent>
+    </Pagination>
+  );
+}
