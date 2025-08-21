a88a3cf6cd74898b719800caa4cabb0630b5f836
Liam Kyle Viray <i_liamkyle.viray@stratpoint.com>
2025-08-22 04:02:03 +0800

    feat: Updated UI of projects in dashboard and card

---
+++ src/components/project-status.tsx
@@ -0,0 +1,16 @@
+import { snakeToTitleCase } from "../lib/utils";
+
+type ProjectStatusProps = {
+  status: string;
+  color: string;
+};
+
+export function ProjectStatus({ status, color }: ProjectStatusProps) {
+  return (
+    <div className={`py-1 px-3 rounded-lg ${color}`}>
+      <span className="text-[15px] font-medium text-white line-clamp-1">
+        {snakeToTitleCase(status)}
+      </span>
+    </div>
+  );
+}
