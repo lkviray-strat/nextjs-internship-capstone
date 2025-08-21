b14e9ea9bbac313f995a36a2f90757e7f205aa55
Liam Kyle Viray <i_liamkyle.viray@stratpoint.com>
2025-08-22 03:56:50 +0800

    chore: Added hydration to trpc client

--- trpc/server.tsx
+++ trpc/server.tsx
@@ -1,14 +1,15 @@
 import "server-only";
 
-import { createTRPCOptionsProxy } from "@trpc/tanstack-react-query";
+import type { AppRouter } from "@/src/types";
+import { createHydrationHelpers } from "@trpc/react-query/rsc";
 import { cache } from "react";
-import { createTRPCContext } from "./init";
+import { createCallerFactory, createTRPCContext } from "./init";
 import { makeQueryClient } from "./query-client";
 import { appRouter } from "./routers/_app";
 
 export const getQueryClient = cache(makeQueryClient);
-export const trpc = createTRPCOptionsProxy({
-  ctx: createTRPCContext,
-  router: appRouter,
-  queryClient: getQueryClient,
-});
+const caller = createCallerFactory(appRouter)(createTRPCContext);
+export const { trpc, HydrateClient } = createHydrationHelpers<AppRouter>(
+  caller,
+  getQueryClient
+);
