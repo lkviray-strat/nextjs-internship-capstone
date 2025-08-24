3ce1ce2264f22d7bc70e6d35d29aa7d0ea330e89
Liam Kyle Viray <i_liamkyle.viray@stratpoint.com>
2025-08-24 18:32:23 +0800

    feat: Added archival and deletion of project on project cards

---
+++ src/components/pages/modals/archive-project-modal.tsx
@@ -0,0 +1,108 @@
+import type { ProjectStatusEnum } from "@/src/types";
+import { useProjects } from "@/src/use/hooks/use-projects";
+import { Trash } from "lucide-react";
+import { useParams } from "next/navigation";
+import React, { useState } from "react";
+import { toast } from "sonner";
+import { ClientOnly } from "../../client-only";
+import { Loader } from "../../loader";
+import { Button } from "../../ui/button";
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogFooter,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "../../ui/dialog";
+import { DropdownMenuItem } from "../../ui/dropdown-menu";
+
+type ArchiveProjectModalProps = {
+  id?: string;
+};
+
+export function ArchiveProjectModal({ id }: ArchiveProjectModalProps) {
+  const projectHooks = useProjects();
+  const params = useParams();
+  const [open, setOpen] = useState(false);
+
+  const handlePropagation = (e: Event | React.MouseEvent) => {
+    e.stopPropagation();
+    e.preventDefault();
+  };
+
+  const handleClose = (e: React.MouseEvent) => {
+    handlePropagation(e);
+    setOpen(false);
+  };
+
+  const handleSubmit = async () => {
+    const projectId = id ?? params.projectId;
+    const teamId = params.teamId;
+
+    try {
+      if (!projectId || !teamId) return;
+
+      const updateValue = {
+        id: projectId.toString(),
+        teamId: teamId.toString(),
+        status: "archived" as ProjectStatusEnum,
+      };
+
+      await projectHooks.updateProject(updateValue);
+      setOpen(false);
+      toast.success("Project archived successfully");
+    } catch (error) {
+      toast.error("Failed to archive project");
+      setOpen(false);
+      console.log(error);
+    }
+  };
+
+  return (
+    <Dialog
+      open={open}
+      onOpenChange={setOpen}
+    >
+      <DialogTrigger asChild>
+        <DropdownMenuItem
+          variant="archive"
+          onSelect={handlePropagation}
+        >
+          <Trash />
+          Archive Project
+        </DropdownMenuItem>
+      </DialogTrigger>
+      <DialogContent className="sm:max-w-[400px]">
+        <DialogHeader>
+          <DialogTitle>Are you absolutely sure?</DialogTitle>
+          <ClientOnly fallback={<Loader />}>
+            <DialogDescription>
+              This action cannot be undone. This will temporarily archive your
+              project and hide your data from the project list.
+            </DialogDescription>
+          </ClientOnly>
+        </DialogHeader>
+        <DialogFooter className="flex flex-col sm:flex-row">
+          <ClientOnly>
+            <Button
+              className="order-2 sm:order-1"
+              variant="outline"
+              onClick={handleClose}
+            >
+              Cancel
+            </Button>
+            <Button
+              className="order-1 sm:order-2"
+              variant="archiveDestructive"
+              onClick={handleSubmit}
+            >
+              Archive
+            </Button>
+          </ClientOnly>
+        </DialogFooter>
+      </DialogContent>
+    </Dialog>
+  );
+}
--- src/components/pages/project/project-card-dropdown.tsx
+++ src/components/pages/project/project-card-dropdown.tsx
@@ -1,7 +1,11 @@
 "use client";
 
-import { Edit, MoreHorizontal, Trash } from "lucide-react";
+import type { Projects, ProjectStatusEnum } from "@/src/types";
+import { useProjects } from "@/src/use/hooks/use-projects";
+import { ArchiveRestore, Edit, MoreHorizontal } from "lucide-react";
 import Link from "next/link";
+import { useParams } from "next/navigation";
+import { toast } from "sonner";
 import { Button } from "../../ui/button";
 import {
   DropdownMenu,
@@ -10,12 +14,39 @@ import {
   DropdownMenuItem,
   DropdownMenuTrigger,
 } from "../../ui/dropdown-menu";
+import { ArchiveProjectModal } from "../modals/archive-project-modal";
+import { DeleteProjectModal } from "../modals/delete-project-modal";
 
 type ProjectCardDropdownProps = {
-  projectId: string;
+  project: Projects;
 };
 
-export function ProjectCardDropdown({ projectId }: ProjectCardDropdownProps) {
+export function ProjectCardDropdown({ project }: ProjectCardDropdownProps) {
+  const projectHooks = useProjects();
+  const { teamId } = useParams();
+
+  const handlePropagation = (e: React.MouseEvent) => {
+    e.stopPropagation();
+  };
+
+  const handleRestoreProject = async () => {
+    if (!teamId || !project.id) return;
+
+    try {
+      const updateValue = {
+        id: project.id,
+        teamId: teamId.toString(),
+        status: "active" as ProjectStatusEnum,
+      };
+
+      await projectHooks.updateProject(updateValue);
+      toast.success("Project restored successfully");
+    } catch (error) {
+      toast.error("Failed to restore project");
+      console.log(error);
+    }
+  };
+
   return (
     <DropdownMenu>
       <DropdownMenuTrigger asChild>
@@ -23,9 +54,7 @@ export function ProjectCardDropdown({ projectId }: ProjectCardDropdownProps) {
           variant="ghost"
           size="icon"
           className="rounded-full"
-          onClick={(e) => {
-            e.stopPropagation();
-          }}
+          onClick={handlePropagation}
         >
           <MoreHorizontal size={16} />
         </Button>
@@ -33,18 +62,28 @@ export function ProjectCardDropdown({ projectId }: ProjectCardDropdownProps) {
       <DropdownMenuContent
         align="start"
         side="right"
+        onClick={handlePropagation}
       >
         <DropdownMenuGroup>
-          <DropdownMenuItem asChild>
-            <Link href={`projects/${projectId}/settings`}>
-              <Edit />
-              Edit Project
-            </Link>
-          </DropdownMenuItem>
-          <DropdownMenuItem className="text-orange-400">
-            <Trash className="text-orange-400" />
-            Archive Project
-          </DropdownMenuItem>
+          {project.status !== "archived" ? (
+            <>
+              <DropdownMenuItem asChild>
+                <Link href={`projects/${project.id}/settings`}>
+                  <Edit />
+                  Edit Project
+                </Link>
+              </DropdownMenuItem>
+              <ArchiveProjectModal id={project.id} />
+            </>
+          ) : (
+            <>
+              <DropdownMenuItem onSelect={handleRestoreProject}>
+                <ArchiveRestore />
+                Restore Project
+              </DropdownMenuItem>
+              <DeleteProjectModal id={project.id} />
+            </>
+          )}
         </DropdownMenuGroup>
       </DropdownMenuContent>
     </DropdownMenu>
