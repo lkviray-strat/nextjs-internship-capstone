c5f442bbaaff71d44d1304bf6cdca1900d71c960
Liam Kyle Viray <i_liamkyle.viray@stratpoint.com>
2025-08-26 01:42:51 +0800

    feat: Added kanban with column dragging

--- src/app/(root)/[teamId]/projects/[projectId]/page.tsx
+++ src/app/(root)/[teamId]/projects/[projectId]/page.tsx
@@ -1,9 +1,11 @@
+import { Kanban } from "@/src/components/kanban-board";
 import { ProjectHeader } from "@/src/components/pages/project/project-header";
 
 export default function ProjectPage() {
   return (
-    <div className="flex flex-col space-y-6 h-full overflow-y-auto">
+    <div className="flex flex-col space-y-8">
       <ProjectHeader />
+      <Kanban />
     </div>
   );
 }
